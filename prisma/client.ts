1be15a1aefd2f6115c895236c174213001dc1ebd
Mister D. Ace <ode808prod@gmail.com>
2025-05-21 19:12:37 +0200

    update prisma.ts

--- prisma/client.ts
+++ prisma/client.ts
@@ -12,3 +12,5 @@ export const prisma = global.prisma || new PrismaClient({
 if (process.env.NODE_ENV !== 'production') {
   global.prisma = prisma;
 }
+
+export { PrismaClient };
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,15 @@
-import { prisma } from '../../prisma/client';
+// import { prisma } from '../../prisma/client';
 
-export { prisma };
+// export { prisma };
+
+
+// lib/prisma.ts
+import { PrismaClient } from '../../prisma/client'
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
